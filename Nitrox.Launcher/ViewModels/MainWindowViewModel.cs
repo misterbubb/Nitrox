5dfb5c303bde99a1834d89a69112061ab7bdf003
MrBub <106004257+misterbubb@users.noreply.github.com>
2026-05-09 14:19:57 -0400

    Add notifications view to read all launcher notifications (#2722)
    Co-authored-by: Measurity <1107063+Measurity@users.noreply.github.com>

--- Nitrox.Launcher/ViewModels/MainWindowViewModel.cs
+++ Nitrox.Launcher/ViewModels/MainWindowViewModel.cs
@@ -29,6 +29,7 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
     private readonly DialogService dialogService;
     private readonly LaunchGameViewModel launchGameViewModel;
     private readonly Func<Window> mainWindowProvider;
+    private readonly NotificationsViewModel notificationsViewModel;
     private readonly OptionsViewModel optionsViewModel;
     private readonly ServerService serverService;
     private readonly ServersViewModel serversViewModel;
@@ -49,6 +50,7 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
         LaunchGameViewModel launchGameViewModel,
         CommunityViewModel communityViewModel,
         BlogViewModel blogViewModel,
+        NotificationsViewModel notificationsViewModel,
         UpdatesViewModel updatesViewModel,
         OptionsViewModel optionsViewModel,
         ServerService serverService,
@@ -61,6 +63,7 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
         this.serversViewModel = serversViewModel;
         this.communityViewModel = communityViewModel;
         this.blogViewModel = blogViewModel;
+        this.notificationsViewModel = notificationsViewModel;
         this.updatesViewModel = updatesViewModel;
         this.optionsViewModel = optionsViewModel;
         this.serverService = serverService;
@@ -69,7 +72,10 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
         this.RegisterMessageListener<ShowPreviousViewMessage, MainWindowViewModel>(static (message, vm) => vm.BackToAsync(message.RoutableViewModelType));
         this.RegisterMessageListener<NotificationAddMessage, MainWindowViewModel>(static async (message, vm) =>
         {
-            Dispatcher.UIThread.Invoke(() => vm.Notifications.Add(message.Item));
+            Dispatcher.UIThread.Invoke(() =>
+            {
+                vm.Notifications.Add(message.Item);
+            });
             await Task.Delay(7000);
             WeakReferenceMessenger.Default.Send(new NotificationCloseMessage(message.Item));
         });
@@ -79,7 +85,10 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
             await Task.Delay(1000); // Wait for animations
             if (!IsDesignMode) // Prevent design preview crashes
             {
-                vm.Notifications.Remove(message.Item);
+                Dispatcher.UIThread.Invoke(() =>
+                {
+                    vm.Notifications.Remove(message.Item);
+                });
             }
         });
 
@@ -121,6 +130,9 @@ internal partial class MainWindowViewModel : ViewModelBase, IRoutingScreen
 
     [RelayCommand(AllowConcurrentExecutions = false)]
     public async Task OpenBlogViewAsync() => await this.ShowAsync(blogViewModel);
+
+    [RelayCommand(AllowConcurrentExecutions = false)]
+    public async Task OpenNotificationsViewAsync() => await this.ShowAsync(notificationsViewModel);
 
     [RelayCommand(AllowConcurrentExecutions = false)]
     public async Task OpenUpdatesViewAsync() => await this.ShowAsync(updatesViewModel);
