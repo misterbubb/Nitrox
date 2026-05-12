03a7e677c5f660cc7652110731362a5759f5de4f
misterbubb <dillonmcm296@gmail.com>
2026-05-11 22:26:40 -0500

    Prevent duplicate entries in ScannerComplete (#2666)

--- Nitrox.Server.Subnautica/Models/GameLogic/PdaManager.cs
+++ Nitrox.Server.Subnautica/Models/GameLogic/PdaManager.cs
@@ -153,7 +153,14 @@ internal sealed class PdaManager(ILogger<PdaManager> logger) : ISummarize
             if (fullyResearched)
             {
                 PdaState.ScannerPartial.RemoveAllFast(techType, static (entry, toRemove) => entry.TechType.Equals(toRemove));
-                PdaState.ScannerComplete.Add(techType);
+                if (!PdaState.ScannerComplete.Contains(techType))
+                {
+                    PdaState.ScannerComplete.Add(techType);
+                }
+                else
+                {
+                    logger.ZLogDebug($"Tech type [{techType.Name}] is already in {nameof(PdaState.ScannerComplete)}, skipping duplicate add");
+                }
             }
             else
             {
