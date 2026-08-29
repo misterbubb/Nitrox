ae3358a3a811646f186f313a04e9a9cf7c20c2f3
misterbubb <dillonmcm296@gmail.com>
2026-08-29 12:43:13 -0500

    Fix LiveMixin entity destruction not syncing (#1701)

--- NitroxPatcher/Patches/Dynamic/LiveMixin_Kill_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/LiveMixin_Kill_Patch.cs
@@ -20,15 +20,25 @@ public sealed partial class LiveMixin_Kill_Patch : NitroxPatch, IDynamicPatch
 
         // We don't broadcast if we don't have objectId or if the object is whitelisted,
         // in which case kill broadcast is managed differently
-        if (!__instance.TryGetNitroxId(out NitroxId objectId) ||
-            Resolve<LiveMixinManager>().IsWhitelistedUpdateType(__instance))
+        if (!__instance.TryGetNitroxId(out NitroxId objectId))
+        {
+            return;
+        }
+
+        if (Resolve<LiveMixinManager>().IsWhitelistedUpdateType(__instance))
         {
             return;
         }
         
-        // Some objects don't have destroyOnDeath but we still need to broadcast the death
-        // (because the destruction is managed by another script)
-        if (__instance.destroyOnDeath || Resolve<LiveMixinManager>().ShouldBroadcastDeath(__instance))
+        // Broadcast destruction if any of these conditions are true:
+        // 1. destroyOnDeath: LiveMixin will destroy the object directly
+        // 2. broadcastKillOnDeath: The object broadcasts OnKill message (default true) which other scripts use for destruction
+        // 3. passDamageDataOnDeath: Similar to broadcastKillOnDeath but passes damage data
+        // 4. ShouldBroadcastDeath: Whitelist for specific entities (e.g., Crash fish)
+        if (__instance.destroyOnDeath || 
+            __instance.broadcastKillOnDeath || 
+            __instance.passDamageDataOnDeath || 
+            Resolve<LiveMixinManager>().ShouldBroadcastDeath(__instance))
         {
             Resolve<IPacketSender>().Send(new EntityDestroyed(objectId));
         }
